6875189e230b92d5cff10f875998ca12088c1d2f
Singh01 <komal94516@gmail.com>
2013-08-07 21:06:00 +0530

    modified web.js

--- web.js
+++ web.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-var http=require('http');
 var fs=require('fs');
-fs.readFileSync('index.html',function(err,data){
-    if(err){
-	throw err;
-    }
+var content=fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-    response.send(data);
-});
+    response.send(content);
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
